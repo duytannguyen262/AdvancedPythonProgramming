797b2628f45df99c3bce0d150f320337a80399e9
Duy Tan <nguyentanduy02062000@gmail.com>
2021-09-14 19:50:01 +0700

    first commit

---
+++ Lab01/bai4.py
@@ -0,0 +1,3 @@
+def absolute_difference(number1, number2):
+ return abs(number1 - number2)
+print(absolute_difference(3, 7))
---
+++ Lab01/bai6.py
@@ -0,0 +1,4 @@
+def average_grade(grade1, grade2, grade3):
+ return (grade1 + grade2 + grade3) / 3
+
+print(average_grade(80, 95, 90))
